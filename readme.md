b5b11e4a43e5951085ee494d11ea358a4994cbdf
Akiva Silver <akiva.silver@gmail.com>
2019-04-11 16:39:32 -0400

    finished all the features
    all the features are finished, they have not been tested but i'll update the repo if need be

--- readme.md
+++ readme.md
@@ -1,3 +1,53 @@
 Chat spammer
 ============================
 this is just a simple chat spammer bot with some configuration
+
+##Features
+- very configurable
+- auto reconnecting
+- anti-afk measures
+- randomization
+- light weight
+
+##Tutorial
+###Requirements
+- Python 2.7 or 3.x
+- pip (comes by default with python)
+
+###Installation
+- open a command window inside the folder of the project
+- run ```pip install -r requirements.txt```
+
+###Configuration
+####config.json
+- account
+    - username (required if not online)
+    - email (required for online)
+    - password (required for online)
+    - online (whether to join in online or offline mode)
+- server
+    - host (server ip address)
+    - port (server port, default 25565)
+- walk (anti afk measure)
+    - distance (in blocks, will return back to starting position)
+    - speed (in ticks, lower means faster)
+- spawn mode
+    - enabled (true or false)
+    - speed (in ticks, lower means faster)
+- respawn_speed (in ticks, how fast should the bot respawn if killed)
+- anti_afk (in ticks, how fast should go through the anti-afk steps)
+- message_speed (in ticks, how often to sent a new message)
+- rng_events (array of all events that should be randomized, all randomizable events are respawn_speed, anti_afk, message_speed)
+- prefix (what you want your messages to start with)
+
+####messages.csv
+its a standard csv with labeled columns
+- prefix_flag
+    - never (messages will never have a prefix)
+    - always (message will always have a prefix)
+    - random (when the message is sent it has a random chance to show or hide the prefix)
+- message (any message you want)
+
+###Execution
+- in the project folder run ```python start.py```
+
--- start.py
+++ start.py
@@ -1,12 +1,16 @@
 import json
 import csv
+import glm
 
 from twisted.internet import defer, reactor
 from quarry.net.client import ClientFactory, SpawningClientProtocol
 from quarry.net.auth import Profile, OfflineProfile
+import random
+
+from twisted.internet.protocol import ReconnectingClientFactory
 
 with open("config.json") as config_file:
-	config = json.load(config_file).data
+	config = json.load(config_file)
 
 messages = []
 
@@ -17,16 +21,145 @@ with open("messages.csv", newline="") as _messages_csv:
 
 
 class SpammerProtocol(SpawningClientProtocol):
+	def __init__(self, *args, **kwargs):
+		super(SpammerProtocol, self).__init__(*args, **kwargs)
+		self.player_position = glm.vec3(0, 0, 0)
+		
+		self.loop = {
+			"afk": True,
+			"message": True
+		}
+		
+		self.crouched = False
+		self.walk_cycle_running = False
+		self.player_eid = -1
+		
 	def player_joined(self):
-		print("connected")
 		super(SpammerProtocol, self).player_joined()
-		self.ticker.add_loop(config["anti_afk"], self.anti_afk)
+		
+		print("connected")
+		
+		self.ticker.add_delay(50, self.anti_afk)
+		self.ticker.add_delay(50, self.random_message)
+		
+		if config["spawn_mode"]["enabled"]:
+			self.ticker.add_loop(config["spawn_mode"]["speed"], self.toggle_crouch)
 		
 	def anti_afk(self):
-		print("anti-afk script ran")
+		if not self.loop["afk"]:
+			return
+		
+		self.walk_cycle()
+		self.send_packet("use_item", self.buff_type.pack_varint(random.choice([0, 1])))
+		self.send_packet("held_item_change", self.buff_type.pack("h", random.randint(0, 8)))
+		
+		# getting loop interval every time instead of getting once
+		self.ticker.add_delay(self.get_delay("anti_afk"), self.anti_afk)
+		
+	def walk_cycle(self):
+		if self.walk_cycle_running:
+			return
+		
+		self.walk_cycle_running = True
+		
+		limit = config["walk"]["walk_distance"]
+		direction = random.choice([glm.vec3(1, 0, 0), glm.vec3(-1, 0, 0), glm.vec3(0, 0, 1), glm.vec3(0, 0, -1)])
+		
+		global sequence
+		sequence = []
+		
+		for _ in range(limit):
+			sequence.append(direction)
+		
+		for _ in range(limit):
+			sequence.append(direction * glm.vec3(-1, 1, -1))
+			
+		def walk(position):
+			x, y, z = self.player_position = self.player_position - position
+			self.pos_look[0] = x
+			self.pos_look[1] = x
+			self.pos_look[2] = x
+			self.send_packet("player_position", self.buff_type.pack("ddd?", x, y, z, True))
+		
+		global index
+		index = 0
+		
+		def ticker_loop():
+			global index
+			global sequence
+			
+			walk(sequence[index])
+			
+			if not index + 1 > sequence.__len__() - 1:
+				index += 1
+				self.ticker.add_delay(config["walk"]["speed"], ticker_loop)
+			else:
+				self.walk_cycle_running = False
+		
+		ticker_loop()
+		
+	def random_message(self):
+		if not self.loop["message"]:
+			return
+		
+		msg_index = random.randint(0, messages.__len__() - 1)
+		prefix_flag, msg = messages[msg_index]
+		
+		if prefix_flag == "random":
+			show_prefix = bool(random.getrandbits(1))
+		else:
+			show_prefix = prefix_flag == "always"
+			
+		output = ""
+		if show_prefix:
+			output += config["prefix"]
+		
+		output += msg
+		self.send_packet('chat_message', self.buff_type.pack_string(output))
+		self.logger.debug("sending message: %s" % output)
+		
+		# getting loop interval every time instead of getting once
+		self.ticker.add_delay(self.get_delay("message_speed"), self.random_message)
+	
+	def toggle_crouch(self):
+		if self.crouched:
+			new_action = 1
+		else:
+			new_action = 0
+			
+		self.send_packet("entity_action", self.buff_type.pack_varint(self.player_eid), self.buff_type.pack_varint(new_action), self.buff_type.pack_varint(0))
+		
+		self.crouched = not self.crouched
+	
+	def respawn(self):
+		self.send_packet("client_status", self.buff_type.pack_varint(0))
+		
+	@staticmethod
+	def get_delay(name):
+		if name in config["rng_events"]:
+			return random.randint(0, config[name])
+		return config[name]
+		
+	# packet handling
+	def packet_update_health(self, buff):
+		hp = buff.unpack("f")
+		buff.discard()
+
+		if hp <= 0:
+			self.ticker.add_delay(self.get_delay("respawn_speed"), self.respawn)
+			
+	def packet_player_position_and_look(self, buff):
+		super(SpammerProtocol, self).packet_player_position_and_look(buff)
+		self.player_position = glm.vec3(self.pos_look[0], self.pos_look[1], self.pos_look[2])
+	
+	def packet_join_game(self, buff):
+		buff.save()
+		self.player_eid = buff.unpack_varint()
+		buff.restore()
+		super(SpammerProtocol, self).packet_unhandled(buff, "join_game")
 		
 
-class SpammerFactory(ClientFactory):
+class SpammerFactory(ClientFactory, ReconnectingClientFactory):
 	protocol = SpammerProtocol
 	
 	def __init__(self, *args, **kwargs):
